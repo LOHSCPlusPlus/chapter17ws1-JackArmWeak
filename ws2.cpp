1abe7edf974be618154db16f7f23ec158df50106
JackArmstrong22 <>
2022-05-31 07:06:40 +0000

    Done

---
+++ ws2.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+// Part 1.1 Complete the node declaration
+struct Node {
+	Node(int val);
+	int data;
+	Node *next;
+};
+
+Node::Node(int val){
+	data = val;
+  next = nullptr;
+};
+
+// Part 1.2 Define the Node constructor
+
+//constants
+const int SENTINEL = -999;
+
+//print function
+void printList(Node *head);
+
+//main
+int main()
+{
+    Node *head = nullptr;
+    Node *tail = nullptr;
+    int num = 0;
+    cout << "Enter a number to add to the list (" << SENTINEL << " to end)";
+    cin >> num;
+    while (num != SENTINEL) {
+        // Part 2, create a new node pointer, and append it to the list.
+        Node *nodePtr = new Node(num);
+        if (head == nullptr) {
+          tail = nodePtr;
+        }
+        else {
+          nodePtr->next = head;
+        }
+        head = nodePtr;
+        cout << "Enter a number to add to the list (" << SENTINEL << " to end)";
+        cin >> num;
+    }
+
+	printList(head);
+
+    if (head != nullptr) {
+      Node *next = head->next;
+	  delete head;
+	  head = next;
+    }
+    if (head == nullptr) {
+      tail = nullptr;
+    }
+
+    // Part 3, delete the list
+    while(head != nullptr) {
+	    Node *next = head->next;
+	    delete head;
+	    head = next;
+    }
+    tail = nullptr;
+
+    return 0;
+}
+
+// Part 4 fill in the print function to print the list
+void printList(Node *head) {
+  Node *current = head;
+  while(current != nullptr) {
+    cout << current -> data << endl;
+    current = current -> next;
+  }
+}
